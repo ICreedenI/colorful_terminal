4bdf5d96d1f65814a3b9f8678b5e3264f50c38ef
ICreedenI <andre.herber.programming@gmail.com>
2023-01-07 19:39:07 +0100

    formating

--- colorful_terminal/__init__.py
+++ colorful_terminal/__init__.py
@@ -1,3 +1,15 @@
-from .definitions import Fore, Back, Style, TermAct, colored_print, demo_print, foreground_demo, background_demo, style_demo, rgb_demo, rainbow_demo
+from .definitions import (
+    Fore,
+    Back,
+    Style,
+    TermAct,
+    colored_print,
+    demo_print,
+    foreground_demo,
+    background_demo,
+    style_demo,
+    rgb_demo,
+    rainbow_demo,
+)
 
-__version__ = '0'
+__version__ = "0.0.2"
